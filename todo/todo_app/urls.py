0ba565ba3b163ae60a0c670535e2eb277db378a7
Богдан Морозюк <bogdanmorozuk72@gmail.com>
2023-01-09 17:08:56 +0200

    login/logout logic added

--- todo/todo_app/urls.py
+++ todo/todo_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='all_tasks'),
+    path('login/', Login.as_view(), name='login-task'),
+    path('logout/', LogoutView.as_view(next_page='login-task'), name='logout-task'),
     path('task/<int:pk>', TaskDetail.as_view(), name='detail_task'),
     path('create-task/', TaskCreate.as_view(), name='create_task'),
+    path('update-task/<int:pk>', TaskUpdate.as_view(), name='update_task'),
+    path('delete-task/<int:pk>', TaskDelete.as_view(), name='delete_task'),
 ]
--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from .models import Tasks
+
+
+class Login(LoginView):
+    template_name = 'todo_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('all_tasks')
 
 
 class TaskList(ListView):
@@ -23,3 +32,17 @@ class TaskCreate(CreateView):
     fields = '__all__'
     success_url = reverse_lazy('all_tasks')
     template_name = 'todo_app/task_form.html'
+
+
+class TaskUpdate(UpdateView):
+    model = Tasks
+    fields = '__all__'
+    success_url = reverse_lazy('all_tasks')
+    template_name = 'todo_app/task_form.html'
+
+
+class TaskDelete(DeleteView):
+    model = Tasks
+    context_object_name = 'task'
+    success_url = reverse_lazy('all_tasks')  # redirects user to main page after post
+    template_name = 'todo_app/task_confirm_delete.html'
